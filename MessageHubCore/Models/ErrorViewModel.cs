4d2f60ff1727928739e7837f3af8e010121bd0d6
westfive <chenlong5469@163.com>
2017-09-19 00:55:00 +0800

    first commit

---
+++ MessageHubCore/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace MessageHubCore.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ SignalRClient/Program.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.SignalR.Client;
+using System;
+
+namespace SignalRClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Press AnyKey To Start");
+            HubClientInit();
+            Console.ReadKey();
+        }
+
+        private static void HubClientInit()
+        {
+            //throw new NotImplementedException();
+            var connection = new HubConnectionBuilder()
+                .WithUrl("http://localhost:61547/messagehub")
+                .WithConsoleLogger()
+                .Build();
+
+            connection.On<string>("Send", data =>
+            {
+                Console.WriteLine($"Received: {data}");
+            });
+
+            connection.StartAsync();
+
+            string str = "Hello";
+            connection.InvokeAsync("Send",str);
+            //connection.InvokeAsync("Ping");
+        }
+    }
+}
